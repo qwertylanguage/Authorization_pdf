cf03ee50ac308f60fbe727baf1234105ef8923b2
qwertylanguage <101062964+qwertylanguage@users.noreply.github.com>
2023-01-09 16:04:44 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print('1')
+from PyPDF2 import PdfFileWriter, PdfFileReader
+from getpass import getpass
+
+pdfwriter = PdfFileWriter()
+pdf = PdfFileReader('Шпаргалка.pdf')
+
+for page in range(pdf.numPages):
+    pdfwriter.add_page(pdf.pages[page])
+
+password = getpass(prompt = "Enter password: ")
+pdfwriter.encrypt(password)
+
+with open("protected.pdf",'wb') as file:
+    pdfwriter.write(file)
